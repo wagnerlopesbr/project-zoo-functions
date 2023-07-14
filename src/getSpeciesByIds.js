b139c5c7d34234f5b6e91cb5ccf244dee95d0bfc
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-07-14 19:12:35 -0300

    WL 1/14 v1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
-};
+const getSpeciesByIds = (...ids) => ids
+  .map((id) => data.species
+    .find((specie) => specie.id === id));
 
 module.exports = getSpeciesByIds;
+
+console.log(getSpeciesByIds());
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
