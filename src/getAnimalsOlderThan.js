3f36d8fa341722ed063ea7b8cc09425875475263
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-07-14 19:41:04 -0300

    WL 2/14 v1

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  if (!(data.species.find((specie) => specie.name === animal))) {
+    return false;
+  }
+  return data.species
+    .find((specie) => specie.name === animal).residents
+    .every((resident) => resident.age >= age);
 };
 
 module.exports = getAnimalsOlderThan;
+
+console.log(getAnimalsOlderThan('otters', 7));
+console.log(getAnimalsOlderThan('penguins', 10));
