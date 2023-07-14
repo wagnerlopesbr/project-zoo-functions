cfed50a2dcc9484de75e4b452c1e5f9e4e1480d5
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-07-14 19:14:29 -0300

    WL 1/14 v2

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -8,4 +8,5 @@ module.exports = getSpeciesByIds;
 
 console.log(getSpeciesByIds());
 console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
+  'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
