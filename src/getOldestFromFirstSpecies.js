bcc1a1b5746a677748f4589d4efd3b3b3be1bf01
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-07-15 11:24:39 -0300

    WL 10/14 v1

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
+function checkingAnimalAge(animalId) {
+  const { species } = data;
+  const animals = species.find((specie) => specie.id === animalId);
+  const oldest = animals.residents.reduce((a, b) => (a.age > b.age ? a : b));
+  return oldest;
+}
+
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const { employees } = data;
+  const employeesId = employees.find((employee) => employee.id === id);
+  const firtSpecieId = employeesId.responsibleFor[0];
+  const { name, sex, age } = checkingAnimalAge(firtSpecieId);
+  return [name, sex, age];
 };
 
 module.exports = getOldestFromFirstSpecies;
