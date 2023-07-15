d349203a42348f78b99bb1526601036c35cd7d1d
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-07-15 08:45:52 -0300

    WL 8/14 v1

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,32 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const visitors = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  entrants.forEach((entrance) => {
+    if (entrance.age < 18) {
+      visitors.child += 1;
+    } else if (entrance.age < 50) {
+      visitors.adult += 1;
+    } else {
+      visitors.senior += 1;
+    }
+  });
+  return visitors;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  const childMoney = child * data.prices.child;
+  const adultMoney = adult * data.prices.adult;
+  const seniorMoney = senior * data.prices.senior;
+  return childMoney + adultMoney + seniorMoney;
 };
 
 module.exports = { calculateEntry, countEntrants };
