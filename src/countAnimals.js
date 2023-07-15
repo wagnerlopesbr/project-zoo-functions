49140ed5cffbf813bb163f967e5d9676ca97a246
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-07-14 21:23:25 -0300

    WL 5/14 v1

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+function totalForSpecie() {
+  data.species.forEach((specie) => new Array([specie.name]) === specie.residents.length);
+  return Array;
+}
+
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    return totalForSpecie();
+  }
+  const { species, sex } = animal;
+  const specieResidents = data.species
+    .find((specie) => specie.name === species).residents;
+  return !sex
+    ? specieResidents.length
+    : species.residents.reduce((acc, resident) => (resident.sex === sex ? acc + 1 : acc), 0);
 };
 
 module.exports = countAnimals;
+
+console.log(countAnimals('lions'));
